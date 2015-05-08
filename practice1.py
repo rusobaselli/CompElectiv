1334bb2a6362d2b52dd2cf55c5d882f3dec5e078
rusobaselli <pablobaselli29@gmail.com>
2015-05-08 06:33:53 -0700

    Create practice1.py

---
+++ practice1.py
@@ -0,0 +1,14 @@
+name = "Pablo Baselli"
+age = 16
+phone = '099902946'
+mail = 'pablobaselli29@gmail.com'
+sex = 'male'
+
+print name 
+print age 
+print phone 
+print mail
+print sex 
+
+print 'My name is' , name , 'I am' , age , 'years old, and I am a' ,  sex
+print 'My phone number is' , phone, 'and my email is' ,  mail 

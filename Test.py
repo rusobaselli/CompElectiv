b72c55db06428c17c595435032077f662ba2c0b3
rusobaselli <pablobaselli29@gmail.com>
2015-05-08 06:35:40 -0700

    Create Test.py

---
+++ Test.py
@@ -0,0 +1,12 @@
+operand1=95
+operand2=64.5
+
+print "95+64.5=" , operand1 + operand2
+
+print "95-64.5=" , operand1 - operand2
+
+print "95*64.5=" , operand1 *operand2
+
+print "95/64.5=" , operand1 / operand2
+
+print "95%64.5=" , operand1 % operand2

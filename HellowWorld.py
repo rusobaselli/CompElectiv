f88231b72d857c9a3597ee4adef9b66539babd55
rusobaselli <pablobaselli29@gmail.com>
2015-05-08 06:33:12 -0700

    Create HellowWorld.py

---
+++ HellowWorld.py
@@ -0,0 +1,15 @@
+print"Hello World"
+print"My Name is Pablo"
+print 2015 #printing an integer
+print "2015" #pirnting a string 
+print 3.1416 #printing a floating point number 
+
+print 2 + 2 
+print 2 - 2 
+print 2 * 2 
+print 2 / 2 
+print 3 % 2 
+
+print 3.1416 * 2.20 #modulus is the remainder of a division
+print 3.1416 * 2 
+print 10 % 3

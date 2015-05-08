27e1c0e8157bc796fe3e5cadb85411e717d2b014
rusobaselli <pablobaselli29@gmail.com>
2015-05-08 06:30:37 -0700

    Create Exercise.py

---
+++ Exercise.py
@@ -0,0 +1,9 @@
+flow = 10
+myint = 20
+mystring = "hello" 
+
+print flow
+print myint
+print mystring 
+
+print 'My classwork is called', mystring, 'I used to have',flow,'years old, and at that time I wanted to have',myint,'years old.'

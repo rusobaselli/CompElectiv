37e761a8f590ce0527c4750fb2b7d5ccc32e9608
rusobaselli <pablobaselli29@gmail.com>
2015-05-12 07:00:22 -0700

    Create Practice2.py

---
+++ Practice2.py
@@ -0,0 +1,7 @@
+import sys 
+print "My name is" , sys.argv[1]
+print "My last name is", sys.argv[2]
+print "Year of birth", sys.argv[3]
+print "Favorite band ", sys.argv[4]
+print "Favorite movie", sys.argv [5]
+print "Favorite friend", sys.argv [6]

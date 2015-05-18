b002217633235c1a27bd16f0dffe1eaa6882f04c
rusobaselli <pablobaselli29@gmail.com>
2015-05-18 06:52:05 -0700

    Create Quiz2.py

---
+++ Quiz2.py
@@ -0,0 +1,15 @@
+name = raw_input ("What isyour name?")
+Lastname = raw_input ("What is your last name?")
+clas = raw_input ("What is your class?")
+yearofbirth = input ("What is your year birth?")
+
+currentage = 2015 - yearofbirth 
+print "I am", currentage, "years old."
+
+Computergrade1s = input ("What is your grade in the 1st semester?")
+Computergrade2s = input ("What is your grade in the 2st semester?")
+
+finalgrade = (Computergrade1s + Computergrade2s) / 2 
+print "My final grade is",finalgrade ,"in the CompElective class."
+
+print "My name is", name, "my last name is", Lastname, ", Im in", clas, "and my final grade is", finalgrade
